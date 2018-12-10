5104c9df4a87bf206542a7a99bebcf5db39cc6a1
Mohamed Boulahia <Mohamed.sbmsb@gmail.com>
2018-12-10 13:32:48 +0100

    Commit  Hello World

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root-sofrecom',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'demoangular0hello';
+  name = 'Mohamed';
+  email = 'mohamed.sbmsb@gmail.com ';
+}
