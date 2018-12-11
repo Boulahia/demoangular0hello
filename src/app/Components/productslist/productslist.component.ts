3bd0c68f7537d26884e0fe7d2fcab8e684c6bd7e
Mohamed Boulahia <Mohamed.sbmsb@gmail.com>
2018-12-11 09:50:23 +0100

    Ajout interface 11 12 2018 09 49

--- src/app/Components/productslist/productslist.component.ts
+++ src/app/Components/productslist/productslist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/Domain/iproduct';
 
 @Component({
   selector: 'app-productslist',
@@ -7,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductslistComponent implements OnInit {
 
-  data = [
+  data: IProduct [] = [
     {'title': 'Disuqe extrne USB', 'unitPrise': 250, 'quantity': 2},
     {'title': 'Sorie Optique', 'unitPrise': 10.5, 'quantity': 5},
-    {'title': 'Ecran Led 22 Pouce', 'unitPrise': 350, 'quantity': 12}
+    {'title': 'Ecran Led 22 Pouce', 'unitPrise': 350, 'quantity': 12},
+    {'title': 'Dell 3050', 'unitPrise': 1200, 'quantity': 12}
 ];
 
   constructor() { }
